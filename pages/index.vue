b8f92490d6e0337e8000435bf5f87944542e149c
Toni Kleinfeld <tonikleinfeld@mbp-von-toni.speedport.ip>
2024-12-29 13:08:18 +0100

    update

--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,31 @@
         Hello from me!
     </div>
 </template>
+
+<script setup>
+useSeoMeta({
+  title: 'Test main Page',
+  description: 'This is a test index page',
+  ogTitle: 'Test main Page',
+  ogDescription: '[og:description]',
+  ogImage: '[og:image]',
+  ogUrl: '[og:url]',
+  twitterTitle: 'Test main Page',
+  twitterDescription: '[twitter:description]',
+  twitterImage: '[twitter:image]',
+  twitterCard: 'summary'
+})
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png'
+    }
+  ]
+})
+</script>
